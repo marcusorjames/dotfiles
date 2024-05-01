38de6cf47782fee1b555421b025330b226b3fdcf
marcusorjames <marcusorjames@gmail.com>
2024-05-01 09:33:44 +0100

    feat(nvim): Add new mappings

--- private_dot_config/nvim/lua/custom/mappings.lua
+++ private_dot_config/nvim/lua/custom/mappings.lua
@@ -8,8 +8,15 @@ M.custom = {
           print("Spelling " .. (vim.wo.spell and "enabled" or "disabled"))
       end,
       "Toggle spell",
-    }
-  }
+    },
+    ["<leader>qq"] = { "<cmd> cclose <CR>", "Close quickfix" },
+    ["<S-Tab>"] = { "<<", "Tab left" },
+    ["<C-e>"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
+    ["<leader><space>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+  },
+  i = {
+    ["<S-Tab>"] = { "<C-d>", "Tab left" },
+  },
 }
 
 return M
